28c9954ef31ba2185081a2a36f8008a01580dc20
Mohan C M <mohanmanjappa@gmail.com>
2022-11-20 05:03:42 +0000

    raviole: naika 1.0 bring up

--- naika_oriole.mk
+++ naika_oriole.mk
@@ -6,7 +6,7 @@
 
 # Inherit some common Lineage stuff.
 TARGET_DISABLE_EPPE := true
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/naika/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/raviole/aosp_oriole.mk)
@@ -16,7 +16,7 @@ $(call inherit-product, device/google/raviole/device-lineage.mk)
 # Device identifier. This must come after all inclusions
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 6
-PRODUCT_NAME := lineage_oriole
+PRODUCT_NAME := naika_oriole
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
